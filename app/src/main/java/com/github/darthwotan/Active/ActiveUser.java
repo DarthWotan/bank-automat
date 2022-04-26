6f78bc26631180cbeb2b0e919744d2447fe443d0
Robert Zeitter <zeitter.robert@gmail.com>
2022-04-26 20:51:33 +0200

    little changes.

--- app/src/main/java/com/github/darthwotan/Active/ActiveUser.java
+++ app/src/main/java/com/github/darthwotan/Active/ActiveUser.java
@@ -1,4 +1,18 @@
 package com.github.darthwotan.Active;
 
+import com.github.darthwotan.Profile.Profile;
+
 public class ActiveUser {
+    private Profile currentProfile;
+    public ActiveUser(Profile profile){
+        currentProfile = profile;
+    }
+
+    public Profile getCurrentProfile() {
+        return currentProfile;
+    }
+
+    public void setCurrentProfile(Profile currentProfile) {
+        this.currentProfile = currentProfile;
+    }
 }
--- app/src/main/java/com/github/darthwotan/Bank/BankController.java
+++ app/src/main/java/com/github/darthwotan/Bank/BankController.java
@@ -1,4 +1,28 @@
 package com.github.darthwotan.Bank;
 
+import com.github.darthwotan.Active.ActiveUser;
+import com.github.darthwotan.LogIn.LogIn;
+import com.github.darthwotan.Profile.Profile;
+
+import java.util.Scanner;
+
 public class BankController {
+    private LogIn logIn;
+    private ActiveUser activeUser;
+    private Profile profile;
+    public BankController(Profile profile){
+        this.profile = profile;
+        logIn = new LogIn(profile, askForPassword());
+
+    }
+    public void logIn(){
+        if(logIn.checkPass()) activeUser.setCurrentProfile(profile);
+        else askForPassword();
+    }
+
+    public String askForPassword(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your password?");
+        return input.nextLine();
+    }
 }
